c1c978d1ec3be3b0dd252160f7faf7702ecab5e9
vikas1509 <119025906+vikas1509@users.noreply.github.com>
2023-08-01 12:55:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+function findDomlevel(){
+
+	let target = document.getElementById("level");
+let c=1;
+	
+	while(target.parentElement!==null){
+		c++;
+		target=target.parentElement;
+	}
+	alert("the level of the element :"n);
+}
